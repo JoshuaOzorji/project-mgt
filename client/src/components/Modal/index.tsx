08604de1bedd267d7b87c8f9ead3283734f31642
Joshua Ozorji <ozorjijoshua@gmail.com>
2026-03-05 11:04:21 +0100

    timeline feature

--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -1,21 +1,37 @@
 import React from "react";
+import { X } from "lucide-react";
+import ReactDOM from "react-dom";
+import Header from "../Header";
 
 type Props = {
+	children: React.ReactNode;
+	isOpen: boolean;
+	onClose: () => void;
 	name: string;
-	buttonComponent: React.ReactNode;
-	isSmallText?: boolean;
 };
 
-const Header = ({ name, buttonComponent, isSmallText }: Props) => {
-	return (
-		<div className='mb-5 flex w-full items-center justify-between'>
-			<h1
-				className={`${isSmallText ? "text-lg" : "text-2xl"} font-semibold dark:text-white`}>
-				{name}
-			</h1>
-			{buttonComponent}
-		</div>
+const Modal = ({ children, isOpen, onClose, name }: Props) => {
+	if (!isOpen) return null;
+
+	return ReactDOM.createPortal(
+		<div className='fixed inset-0 z-50 flex h-full w-full items-center justify-center overflow-y-auto bg-gray-600 bg-opacity-50 p-4'>
+			<div className='w-full max-w-2xl rounded-lg bg-white p-4 shadow-lg dark:bg-dark-secondary'>
+				<Header
+					name={name}
+					buttonComponent={
+						<button
+							className='flex h-7 w-7 items-center justify-center rounded-full bg-blue-primary text-white hover:bg-blue-600'
+							onClick={onClose}>
+							<X size={18} />
+						</button>
+					}
+					isSmallText
+				/>
+				{children}
+			</div>
+		</div>,
+		document.body,
 	);
 };
 
-export default Header;
+export default Modal;
