8d479c6dc4984a00cc037dd5757597d9899ebd6e
Joshua Ozorji <ozorjijoshua@gmail.com>
2026-03-08 17:24:00 +0100

    boardview

---
+++ client/src/app/projects/BoardView/index.tsx
@@ -0,0 +1,149 @@
+import {
+	useGetTasksQuery,
+	useUpdateTasksStatusMutation,
+} from "@/src/state/api";
+import { TaskType } from "gantt-task-react/dist/types/public-types";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+
+type Props = {
+	id: string;
+	setIsModalNewTaskOpen: (isOpen: boolean) => void;
+};
+
+const taskStatus = ["To Do", "Work in Progress", "Under Review", "Completed"];
+
+const BoardView = ({ id, setIsModalNewTaskOpen }: Props) => {
+	const {
+		data: tasks,
+		isLoading,
+		error,
+	} = useGetTasksQuery({ projectId: Number(id) });
+
+	const [updateTaskStatus] = useUpdateTasksStatusMutation();
+
+	const moveTask = (taskId: number, toStatus: string) => {
+		updateTaskStatus({ taskId, status: toStatus });
+	};
+
+	if (isLoading) return <div>Loading...</div>;
+	if (error) return <div>An error occurred while fetching tasks</div>;
+
+	return (
+		<DndProvider>
+			<div className='grid grid-cols-1 gap-4 p-4 md:grid-cols-2 xl:grid-cols-4'>
+				{taskStatus.map((status) => (
+					<TaskColumn
+						key={status}
+						status={status}
+						tasks={tasks || []}
+						moveTask={moveTask}
+						setIsModalNewTaskOpen={
+							setIsModalNewTaskOpen
+						}
+					/>
+				))}
+			</div>
+		</DndProvider>
+	);
+};
+
+type TaskColumnProps = {
+	status: string;
+	tasks: TaskType[];
+	moveTask: (taskId: number, toStatus: string) => void;
+	setIsModalNewTaskOpen: (isOpen: boolean) => void;
+};
+
+const TaskColumn = ({
+	status,
+	tasks,
+	moveTask,
+	setIsModalNewTaskOpen,
+}: TaskColumnProps) => {
+	const [{ isover }, drop] = useDrop(() => ({
+		accept: "task",
+		drop: (item: { id: number }) => moveTask(item.id, status),
+		collect: (monitor: any) => ({
+			isOver: !!monitor.isOver(),
+		}),
+	}));
+
+	const tasksCount = tasks.filter(
+		(task) => task.status === status,
+	).length;
+
+	const statusColor: any = {
+		"To Do": "#2563EB",
+		"Work In Progress": "#059669",
+		"Under Review": "#D97706",
+		Completed: "#000000",
+	};
+
+	return (
+		<div
+			ref={(instance) => {
+				drop(instance);
+			}}
+			className={`sl:py-4 rounded-lg py-2 xl:px-2 ${isOver ? "bg-blue-100 dark:bg-neutral-950" : ""}`}>
+			<div className='mb-3 flex w-full'>
+				<div
+					className={`w-2 !bg-[${statusColor[status]}] rounded-s-lg`}
+					style={{
+						backgroundColor:
+							statusColor[status],
+					}}
+				/>
+				<div className='flex w-full items-center justify-between rounded-e-lg bg-white px-5 py-4 dark:bg-dark-secondary'>
+					<h3 className='flex items-center text-lg font-semibold dark:text-white'>
+						{status}{" "}
+						<span
+							className='ml-2 inline-block rounded-full bg-gray-200 p-1 text-center text-sm leading-none dark:bg-dark-tertiary'
+							style={{
+								width: "1.5rem",
+								height: "1.5rem",
+							}}>
+							{tasksCount}
+						</span>
+					</h3>
+					<div className='flex items-center gap-1'>
+						<button className='flex h-6 w-5 items-center justify-center dark:text-neutral-500'>
+							<EllipsisVertical
+								size={26}
+							/>
+						</button>
+						<button
+							className='flex h-6 w-6 items-center justify-center rounded bg-gray-200 dark:bg-dark-tertiary dark:text-white'
+							onClick={() =>
+								setIsModalNewTaskOpen(
+									true,
+								)
+							}>
+							<Plus size={16} />
+						</button>
+					</div>
+				</div>
+			</div>
+
+			{tasks
+				.filter((task) => task.status === status)
+				.map((task) => (
+					<Task key={task.id} task={task} />
+				))}
+		</div>
+	);
+};
+
+type TaskProps = {
+	task: TaskType;
+};
+
+const Task = ({ task }: TaskProps) => {
+	const [{ isDragging }, drag] = useDrag(() => ({
+		type: "task",
+		item: { id: task.id },
+		collect: (monitor: any) => ({
+			isDragging: !!monitor.isDragging(),
+		}),
+	}));
+};
+export default BoardView;
